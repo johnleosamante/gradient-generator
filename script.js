55f2bfe5f55c3f7b712254948449c69f5a7e8a14
John Leo Samante <johnleomuitsamante@gmail.com>
2021-01-07 20:20:33 +0800

    Update to newer javascript syntax.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
-setGradient();
-
-function setGradient() {
-  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
+setGradient = () => {
+  body.style.background = linearGradient();
   css.textContent = body.style.background + ";";
 }
+
+linearGradient = () => `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
